623e14f995571beac8557b0597b09490b6ca01dc
Bhanu Kandregula <bkandreg@bhanukandregula.com>
2020-07-28 12:50:22 -0400

    Updated with /getnames endpoint

--- 08-express-get/index.js
+++ 08-express-get/index.js
@@ -8,6 +8,31 @@ app.get("/", (request, response) => {
   response.send("Express server was successfully established.");
 });
 
+app.get("/getnames", (request, response) => {
+  const stringNames = [
+    "Mr. Kandregula",
+    "Ms. Boddeti",
+    "Ms. Karri",
+    "Mr. Surisetty",
+  ];
+
+  const jsonNames = [
+    { name: "Bhanu", age: 28 },
+    {
+      name: "Raghu",
+      age: 26,
+    },
+    {
+      name: "Shyam",
+      age: 23,
+    },
+  ];
+
+  response.send(JSON.stringify(stringNames));
+  // response.send(jsonNames);
+  // response.send(JSON.stringify(jsonNames));
+});
+
 const port = process.env.PORT;
 debug(" this is the running port: ", port);
 app.listen(port, () => {
