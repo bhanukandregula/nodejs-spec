e23f9072a2b27e2b9a3f3090060d4c9ac1b1e56a
Bhanu Prakash Kandregula <bhanuprakashkandregula@Bhanus-MBP.attlocal.net>
2020-09-06 10:42:59 -0400

    Demo for apidoc

---
+++ 17-apidoc/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
+app.get('/', (request, response) => {
+    response.send('Express is running successfully');
+});
+
+app.listen(3000, () => {
+    console.log('Express is running on port 3000');
+});
